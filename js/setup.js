9229ed88edf39bdaaedfc28f07722cf01b2bebf7
Serenya Dyadev <3684182@gmail.com>
2020-06-03 13:14:43 +0300

    исправление по замечаниям

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var familyNames = ['да Марьян', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var FAMILY_NAMES = ['да Марьян', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var NUMBER_PERSONS = 4;
 
 var getRandomElement = function (elements) {
   return elements[Math.round(Math.random() * (elements.length - 1))];
@@ -11,25 +12,25 @@ var getRandomElement = function (elements) {
 
 var getObjectPerson = function () {
   var person = {
-    name: getRandomElement(names) + ' ' + getRandomElement(familyNames),
-    coatColor: getRandomElement(coatColors),
-    eyesColor: getRandomElement(eyesColors)
+    name: getRandomElement(NAMES) + ' ' + getRandomElement(FAMILY_NAMES),
+    coatColor: getRandomElement(COAT_COLORS),
+    eyesColor: getRandomElement(EYES_COLOR)
   };
 
   return person;
 };
 
 // можно добавить параметр для колличества генерируемых персон
-var getArrayPersonages = function () {
+var getArrayPersonages = function (number) {
   var arrayPersonages = [];
-  for (var i = 0; i < 4; i++) {
-    arrayPersonages[i] = getObjectPerson();
+  for (var i = 0; i < number; i++) {
+    arrayPersonages.push(getObjectPerson());
   }
 
   return arrayPersonages;
 };
 
-var personages = getArrayPersonages();
+var personages = getArrayPersonages(NUMBER_PERSONS);
 // Создайте массив, состоящий из 4-х сгенерированных JS объектов, которые будут описывать похожих персонажей. Объекты должны содержать следующие поля:
 
 var userDialog = document.querySelector('.setup');
