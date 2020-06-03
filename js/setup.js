b1d48030a47f16361d0628492ba4ebc98d0df592
Serenya Dyadev <3684182@gmail.com>
2020-06-03 12:32:34 +0300

    отрисовываем полученный массив с данными на странице по заданию

--- js/setup.js
+++ js/setup.js
@@ -1,50 +1,61 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
-// Покажите блок .setup, убрав в JS-коде у него класс hidden.
-
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var familyNames = ['да Марьян', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-
-// var getFullName = function (names, familyNames) {
-//   var name = names[Math.round(Math.random() * (names.length - 1))];
-//   var familyName = familyNames[Math.round(Math.random() * (familyNames.length - 1))];
-//   return (name + ' ' + familyName);
-// };
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var getRandomElement = function (elements) {
   return elements[Math.round(Math.random() * (elements.length - 1))];
 };
 
-var fullName = getRandomElement(names) + ' ' + getRandomElement(familyNames);
-console.log(fullName);
+var getObjectPerson = function () {
+  var person = {
+    name: getRandomElement(names) + ' ' + getRandomElement(familyNames),
+    coatColor: getRandomElement(coatColors),
+    eyesColor: getRandomElement(eyesColors)
+  };
 
-var fullCoatColor = getRandomElement(coatColor);
-console.log(fullCoatColor);
+  return person;
+};
 
-var fullEyesColor = getRandomElement(eyesColor);
-console.log(fullEyesColor);
-// for (var i = 0; i < 4; i++) {
-//   var names = [];
-//   name[i] = getName(names, familyNames);
-// }
+// можно добавить параметр для колличества генерируемых персон
+var getArrayPersonages = function () {
+  var arrayPersonages = [];
+  for (var i = 0; i < 4; i++) {
+    arrayPersonages[i] = getObjectPerson();
+  }
 
-// console.log(name);
-/*
+  return arrayPersonages;
+};
 
+var personages = getArrayPersonages();
+// Создайте массив, состоящий из 4-х сгенерированных JS объектов, которые будут описывать похожих персонажей. Объекты должны содержать следующие поля:
 
-Создайте массив, состоящий из 4-х сгенерированных JS объектов, которые будут описывать похожих персонажей. Объекты должны содержать следующие поля:
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+// Покажите блок .setup, убрав в JS-коде у него класс hidden.
 
-На основе данных, созданных в предыдущем пункте и шаблона #similar - wizard - template создайте DOM - элементы, соответствующие случайно сгенерированным волшебникам, и заполните их данными из массива:
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+// шаблона #similar - wizard - template
 
-имя персонажа name запишите как текст в блок.setup - similar - label;
-цвет мантии coatColor задайте как цвет заливки fill в стилях элемента.wizard - coat;
-цвет глаз eyesColor задайте как цвет заливки fill в стилях элемента.wizard - eyes.
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-Отрисуйте сгенерированные DOM-элементы в блок .setup-similar-list. Для вставки элементов используйте DocumentFragment.
+  return wizardElement;
+};
 
-Покажите блок .setup-similar, удалив у него CSS-класс hidden.
-*/
-// var name =
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < personages.length; i++) {
+  fragment.appendChild(renderWizard(personages[i]));
+}
+
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
