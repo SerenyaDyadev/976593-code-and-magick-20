c35f35f723b53359607e9ec5d5eb5dc39c4f40c4
Serenya Dyadev <3684182@gmail.com>
2020-06-29 17:19:17 +0300

    модуль render.js

---
+++ js/render.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+  var MAX_SIMILAR_WIZARD_COUNT = 4;
+
+  var userDialog = document.querySelector('.setup');
+
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+
+    return wizardElement;
+  };
+
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  window.backend('https://javascript.pages.academy/code-and-magick/data', 'GET', successHandler);
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,36 +1,7 @@
 'use strict';
 
 (function () {
-  var MAX_SIMILAR_WIZARD_COUNT = 4;
-
   var userDialog = document.querySelector('.setup');
-
-  var similarListElement = userDialog.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
-
-  var renderWizard = function (wizard) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-
-    return wizardElement;
-  };
-
-  var successHandler = function (wizards) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
-      fragment.appendChild(renderWizard(window.util.getRandomElement(wizards)));
-    }
-    similarListElement.appendChild(fragment);
-
-    userDialog.querySelector('.setup-similar').classList.remove('hidden');
-  };
-
-  window.backend('https://javascript.pages.academy/code-and-magick/data', 'GET', successHandler);
 
   var form = userDialog.querySelector('.setup-wizard-form');
   form.addEventListener('submit', function (evt) {
